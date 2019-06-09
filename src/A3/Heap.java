f52e08c70d56aa91980cb813ae181721d4484df4
William <w.morin.laberge@gmail.com>
2019-06-09 17:37:15 -0400

    Insert and reconstruct left

--- src/A3/Heap.java
+++ src/A3/Heap.java
@@ -1,12 +1,31 @@
 package A3;
+import java.util.Arrays;
+//only used to compare two arrays for equality for better readability
 
 public class Heap {
 	public boolean isMax;
 	private int size;
 	private int[][] heap;
 	//heap is of the form [[key,value],[key,value],[key,value],...]
+	private static int toRemember;
 	
-	//when accessing heap nodes, it is assumed that the user is only interested in the value of the node, not its key
+
+	public void reconstruct() {
+		
+	}
+	
+	public int[] insert(int k, int v) {
+		int[] toAdd = {k,v};
+		size++;
+		//might need to extend the array representation of the heap
+		//the size has been incremented, so we know the array is full if size == array.length
+		if(size == heap.length)
+			extendHeap();
+		
+		
+		
+		return toAdd;
+	}
 	
 	public Heap(boolean isMax) {
 		this.isMax = isMax;
@@ -42,8 +61,59 @@ public class Heap {
 		return result;
 	}
 	
-	public void reconstruct() {
+	//here node2 will be taken from the heap
+	public int compare(int[] node1, int[] node2) {
+		//you might need some <= or >= in here since a node can have children with equal keys
+		if (isMax) {
+			if(node1[0]>node2[0])
+				return -1;
+			if(node1[0]<=node2[0])
+				return 1;
+		}
+		//this applies if we're dealing with a min heap
+		if(node1[0]<node2[0])
+			return -1;
+		if(node1[0]>=node2[0])
+			return 1;
+		System.err.println("A problem occured!");
+		return -2;
+	}
+	
+	//since this is a heap, and not a binary search tree, it takes O(n) to find the entry,
+	//because we don't know which child of any node is the biggest child node.
+	public int replaceKey(int[] e, int k) {
+		replacerHelper(e,k,0, true);
+		reconstruct();
+		//the key will be stored in that var
+		return toRemember;
+	}
+	
+	public int replaceValue(int[] e, int v) {
+		replacerHelper(e,v,0, false);
+		//no need to reconstruct the tree, no keys were changed
 		
+		//the value will be stored in that var
+		return toRemember;
+	}
+	
+	public void replacerHelper(int[] e, int kv, int current, boolean wantKey) {
+		//it has gone too far
+		if(current>size-1)
+			return;
+		//the desired node was found
+		if(Arrays.equals(e, heap[current])) {
+			if (wantKey) {
+				toRemember = e[0];e[0] = kv;
+			} else {
+				toRemember = e[1];e[1] = kv;
+			}
+			return;
+		}
+		if(compare(e, heap[current])==-1)
+			return;//it is useless to continue searching
+		//visiting the children of the current node
+		replacerHelper(e, kv, current*2+1, wantKey);
+		replacerHelper(e, kv, current*2+2, wantKey);
 	}
 	
 	public int size() {
@@ -67,10 +137,5 @@ public class Heap {
 		reconstruct();
 	}
 	
-	
-	
-	
-	
-		
 
 }
